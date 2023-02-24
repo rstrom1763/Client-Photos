7e1829fe3fefa0c188a07a7bbe58a8c2e3323375
Ryan <48137622+rstrom1763@users.noreply.github.com>
2023-02-24 01:06:42 -0600

    Initial Commit
    Everything that has been done so far.

---
+++ go.mod
@@ -0,0 +1,11 @@
+module main
+
+go 1.19
+
+require github.com/aws/aws-sdk-go v1.44.208
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/image v0.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/disintegration/imaging"
+)
+
+var wg sync.WaitGroup //Create the waitgroup object
+
+func getObjects(client *s3.S3, region string, bucket string, prefix string, maxkeys int64) []string {
+
+	var final []string
+
+	objects, err := client.ListObjects(&s3.ListObjectsInput{
+		Bucket:  &bucket,
+		Prefix:  &prefix,
+		MaxKeys: &maxkeys,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, key := range objects.Contents {
+		if *key.Size > 0 {
+			final = append(final, *key.Key)
+		}
+	}
+	return final
+}
+
+func createUrls(client *s3.S3, bucket string, keys []string, minutes int64) []string {
+
+	var final []string
+
+	for _, key := range keys {
+		req, _ := client.GetObjectRequest(&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+		})
+		urlStr, err := req.Presign(time.Duration(minutes) * time.Minute)
+		if err != nil {
+			log.Println("Failed to sign request", err)
+		}
+		final = append(final, urlStr)
+	}
+
+	return final
+}
+
+func createHTML2(keys []string) string {
+
+	var final string
+
+	final += `<!doctype html>
+	<html lang="en">
+	
+	<head><link rel="stylesheet" href="test.css"><style type="text/css"></style></head><br><br><br><br><br><br><br><br><br><br>
+	
+	<body style="background-color:rgb(168, 168, 168);">
+		<div id="gallery">
+	`
+
+	for _, key := range keys {
+		final += fmt.Sprintf("<img src=\"%v\" class=\"thumbnail\">\n", key)
+	}
+
+	final += "</div></body></html>"
+	return final
+}
+
+func createHTML(keys []string) string {
+
+	var final string
+
+	final += `<!doctype html>
+	<html lang="en">
+	 <head>
+	 <link rel="stylesheet" href="test.css">
+	  <meta charset="utf-8">
+	  
+	  <title>Image Gallery</title>
+	  <meta name="description" content="Responsive Image Gallery">
+	  <meta name="author" content="Tim Wells">
+	  
+	  <style type="text/css">
+	  </style>
+	</head>
+	<body>
+	<div id="gallery">
+	`
+
+	for _, key := range keys {
+		final += fmt.Sprintf("<img src=\"%v\">\n", key)
+	}
+
+	final += `</div>
+ 
+	</body>
+   </html>`
+	return final
+}
+
+func getImageDimension(imagePath string) (int, int) {
+	file, err := os.Open(imagePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+
+	image, _, err := image.DecodeConfig(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
+	}
+	return image.Width, image.Height
+}
+
+func createThumbnail(src string, dst string, height int, width int, quality int) {
+	defer wg.Done() //Schedule with the waitgroup
+
+	var thumbnail image.Image
+
+	var orientation string
+	width1, height1 := getImageDimension(src)
+	if width1 > height1 {
+		orientation = "landscape"
+	} else if height1 > width1 {
+		orientation = "portrait"
+	}
+
+	orig, err := imaging.Open(src)
+	if err != nil {
+		log.Fatalf("Failed to open image: %v", err)
+	}
+
+	if orientation == "landscape" {
+		thumbnail = imaging.Resize(orig, height, width, imaging.Lanczos)
+	} else if orientation == "portrait" {
+		thumbnail = imaging.Resize(orig, width, height, imaging.Lanczos)
+	}
+
+	// Save the resulting image as JPEG.
+	err = imaging.Save(thumbnail, dst, imaging.JPEGQuality(quality))
+	if err != nil {
+		log.Fatalf("Failed to save image: %v", err)
+	}
+}
+
+func thumbnailDir(dir string, height int, width int, quality int) {
+
+	routines := 0
+	start_time := time.Now()
+	photos, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, photo := range photos {
+		if !(strings.Contains(photo.Name(), "_thumb")) {
+			photoPath := fmt.Sprintf("%v/%v", dir, photo.Name())
+			noSuffixName := strings.TrimSuffix(photo.Name(), filepath.Ext(photoPath))
+			thumbnailName := fmt.Sprintf("%v/%v_thumb.jpg", dir, noSuffixName)
+			_, err := os.OpenFile(thumbnailName, os.O_RDWR, 0644)
+			if errors.Is(err, os.ErrNotExist) {
+				if strings.ToLower(filepath.Ext(photoPath)) == ".jpg" {
+					wg.Add(1) //Add the Go routine to the waitlist
+					routines += 1
+					go createThumbnail(photoPath, thumbnailName, height, width, quality)
+					if routines >= 8 {
+						wg.Wait()
+						routines = 0
+					}
+				}
+			}
+		}
+	}
+	wg.Wait() //Wait for all the Go routines to finish
+	duration := time.Since(start_time)
+	fmt.Println(duration)
+}
+
+func main() {
+	thumbnailDir("Z:/san_antonio_zoo/small_final", 1920, 0, 80)
+
+	region := "us-east-2" //AWS region to be used
+	bucket := "ryans-test-bucket423"
+	prefix := "image_host/"
+	//key := "image_host/2D3A4246.jpg"
+	var minutes int64 = 20
+	var maxkeys int64 = 5000
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create S3 service client
+	client := s3.New(sess)
+
+	objects := getObjects(client, region, bucket, prefix, maxkeys)
+	urls := createUrls(client, bucket, objects, minutes)
+	os.WriteFile("./test2.html", []byte(createHTML(urls)), 0644)
+
+}
