3c170c188abd819266806a743163119742ce4bbb
Ryan <48137622+rstrom1763@users.noreply.github.com>
2023-03-05 22:34:17 -0600

    Initial submit feature
    The submit button now will find the selected images and post their names to the api. For now the data is saved into picks.json in the current directory. This will need to be saved to DynamoDB later down the line.

--- api/js.js
+++ api/js.js
@@ -1,5 +1,5 @@
 function markImage(id) {
-    img = document.getElementById(id)
+    var img = document.getElementById(id)
     if (img.alt == "1") {
         img.alt = "0";
         img.childNodes[0].style = null
@@ -8,4 +8,38 @@ function markImage(id) {
         borderPX = Math.floor(img.childNodes[0].width * .0125)
         img.childNodes[0].style = "outline: " + borderPX + "px solid #51f542;outline-offset: -" + borderPX + "px;"
     }
+}
+
+function submit() {
+    var pics = []
+    var gallery = document.getElementById("gallery")
+    gallery = gallery.childNodes
+
+    for (var i = 0; i < gallery.length; i++) {
+
+        if (gallery[i].id != undefined) {
+
+            if (gallery[i].alt == "1") {
+
+                pics.push(gallery[i].id)
+
+            }
+        }
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8081/submit");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            alert("Your selections have been submitted!!!")
+        }
+    };
+
+    xhr.send(JSON.stringify(pics));
+
 }
