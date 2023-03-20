168bea63129f52be8730da83302970a054588e0b
Ryan <48137622+rstrom1763@users.noreply.github.com>
2023-03-19 21:44:24 -0500

    (uploader.go) Use channels instead of waitgroup
    Uploader.go now uses waitgroups to keep the max number of concurrent goroutines. Before, the code would hit the max number of routines then wait until they all finished. Now the goroutines will send a message in the channel that will signal its completion. The loop that is sitting there waiting will recieve that signal and it will then start another goroutine so to always keep the max number running to use the CPU to it's highest potential.

--- uploader/uploader.go
+++ uploader/uploader.go
@@ -44,7 +44,7 @@ func getImageDimension(imagePath string) (int, int) {
 // height is the desired height for the jpg to be resized to
 // width is the desired width for the jpg to be resized to
 // quality is the percentage of quality the jpg should be taken down to. Should be between 1 and 99. Example: 80
-func createThumbnail(src string, dst string, height int, width int, quality int) {
+func createThumbnail(src string, dst string, height int, width int, quality int, respchan chan string) {
 
 	defer wg.Done() //Schedule with the waitgroup
 
@@ -80,7 +80,7 @@ func createThumbnail(src string, dst string, height int, width int, quality int)
 	if err != nil {
 		log.Fatalf("Failed to save image: %v", err)
 	}
-
+	respchan <- "done"
 }
 
 // Creates thumbnails of all of the jpg files in a directory
@@ -90,7 +90,7 @@ func createThumbnail(src string, dst string, height int, width int, quality int)
 // width is the desired width for the jpg to be resized to
 // quality is the percentage of quality the jpg should be taken down to. Should be between 1 and 99. Example: 80
 // maxroutines is the max number of concurrent goroutines you would like at a time. Higher = higher CPU and Memory usage
-func thumbnailDir(dir string, height int, width int, quality int, maxroutines int) {
+func thumbnailDir(dir string, height int, width int, quality int, maxroutines int, respchan chan string) {
 
 	routines := 0            // Number of goroutines
 	start_time := time.Now() // Timer start time
@@ -121,15 +121,23 @@ func thumbnailDir(dir string, height int, width int, quality int, maxroutines in
 				// Only execute on files that are jpg
 				if strings.ToLower(filepath.Ext(photoPath)) == ".jpg" || strings.ToLower(filepath.Ext(photoPath)) == ".jpeg" {
 
-					wg.Add(1)                                                            // Add a Go routine to the waitlist
-					routines += 1                                                        // Add one to the number of active goroutines
-					go createThumbnail(photoPath, thumbnailName, height, width, quality) // Start goroutine to create a thumbnail of the jpg
+					wg.Add(1)                                                                      // Add a Go routine to the waitlist
+					routines += 1                                                                  // Add one to the number of active goroutines
+					go createThumbnail(photoPath, thumbnailName, height, width, quality, respchan) // Start goroutine to create a thumbnail of the jpg
 
 					// If the number of active goroutines reaches the max desired concurrent goroutines, wait for them to finish
 					// Reset counter to zero then continue
 					if routines >= maxroutines {
-						wg.Wait()
-						routines = 0
+						for true {
+
+							chanlen := len(respchan)
+							if chanlen > 0 {
+								fmt.Println(<-respchan)
+								routines -= 1
+								break
+							}
+							time.Sleep(10 * time.Millisecond)
+						}
 					}
 				}
 			}
@@ -144,12 +152,15 @@ func main() {
 
 	args := os.Args
 	good_arg_len := 6
-	if len(args) < good_arg_len || len(args) > good_arg_len{
+	respchan := make(chan string, 100)
+	defer close(respchan)
+
+	if len(args) < good_arg_len || len(args) > good_arg_len {
 		log.Fatal("Usage: dir, height, width, quality, maxroutines")
 	}
 	height, _ := strconv.Atoi(args[2])
 	width, _ := strconv.Atoi(args[3])
 	quality, _ := strconv.Atoi(args[4])
 	maxroutines, _ := strconv.Atoi(args[5])
-	thumbnailDir(args[1], height, width, quality, maxroutines)
+	thumbnailDir(args[1], height, width, quality, maxroutines, respchan)
 }
