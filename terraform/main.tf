7c2695085145d84fd894f1a3287d5abecc4d5376
Ryan <48137622+rstrom1763@users.noreply.github.com>
2026-03-28 23:25:54 -0700

    Began implementing Terraform for AWS resources

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource aws_dynamodb_table "request_logs" {
+
+  name = "request_logs"
+  billing_mode = "PAY_PER_REQUEST"
+  deletion_protection_enabled = true
+  hash_key = "request_id"
+  range_key = "timestamp"
+
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "request_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "N"
+  }
+
+  attribute {
+    name = "method"
+    type = "S"
+  }
+
+  attribute {
+    name = "status"
+    type = "N"
+  }
+
+  replica {
+    region_name = "eu-central-1"
+    consistency_mode = "EVENTUAL"
+  }
+
+  global_secondary_index {
+
+    key_schema {
+      attribute_name = "method"
+      key_type = "HASH"
+    }
+
+    key_schema {
+      attribute_name = "status"
+      key_type = "RANGE"
+    }
+
+    key_schema {
+      attribute_name = "timestamp"
+      key_type = "RANGE"
+    }
+
+    name            = "method_gsi"
+    projection_type = "ALL"
+  }
+
+}
